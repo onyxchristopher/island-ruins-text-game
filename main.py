023743d50a87a13d5cfd25047c551484dc3b3680
onyxc <onyx.christopher@mail.utoronto.ca>
2022-04-01 19:06:03 -0400

    First text added

---
+++ main.py
@@ -0,0 +1,31 @@
+print("Your boat washes up against some rocks, nearly capsizing as the strong" +
+      " waves batter it again and again. Thankfully, it dislodges, and you" +
+      " reach the shore. You haul your tiny craft onto the sand, well out of" +
+      " reach of the lapping waves. Now to answer the most pressing question:" +
+      " where are you?")
+
+print("tutorial tells you to type *look around*")
+
+print("You climb up onto some rocks to *look around*. The dense forest in" +
+      " front of you slopes slowly towards the peak of a wide but short" +
+      " mountain. You see a village further along the beach, the buildings" +
+      " clustered around a couple of piers which extend into a large inlet." +
+      " Opposite the village is a large peninsula jutting into the other side" +
+      " of the same inlet. Upon it are some ancient ruins. A fascinating" +
+      " subject- your archeologist's heart wars with practicality as you" +
+      " decide where to go first. You have enough food to last about a week," +
+      " but your boat isn't steerable or large enough to get off this island." +
+      " On the other hand, an ill-fated trip to map a different ruin was what" +
+      " stranded you here in the first place. Do you *explore the village* or" +
+      " *explore the ruins*?")
+
+print("at the village")
+
+print("You grab your bag, compass, and food, and set out. The walk along" +
+      " the beach is uneventful, and within ten minutes you come to a river." +
+      " Thankfully, it seems the locals ford it enough to have built a" +
+      " bridge. After crossing, the village looms ahead of you. Even with" +
+      " the sun overhead, the market appears nearly deserted, and only a few" +
+      " small fishing boats sit next to the piers. Not exactly a bustling" +
+      " port town, then. How are you going to go about acquiring passage off" +
+      " this island?")
